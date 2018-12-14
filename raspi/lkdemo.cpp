88d1ee520df0ca9755fa47afcbe6416e41c4e790
RB <maxx04gmx.de>
2018-12-14 18:06:18 +0100

    fuc

--- raspi/lkdemo.cpp
+++ raspi/lkdemo.cpp
@@ -1,4 +1,5 @@
-
+#include <iostream>
+#include <string>
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
@@ -6,55 +7,58 @@
 #include "../UDP_Base.h"
 #include "follower.h"
 
-
-//static void help()
-//{
-//    // print a welcome message, and the OpenCV version
-//    cout << "\nThis is a demo of Lukas-Kanade optical flow lkdemo(),\n"
-//            "Using OpenCV version " << CV_VERSION << endl;
-//    cout << "\nIt uses camera by default, but you can provide a path to video as an argument.\n";
-//    cout << "\nHot keys: \n"
-//            "\tESC - quit the program\n"
-//            "\tr - auto-initialize tracking\n"
-//            "\tc - delete all the points\n"
-//			"\ts - set aim point\n"
-//			"\tk - calibrate camera\n"
-//             << endl;
-//}
+using namespace cv;
+using namespace std;
 
 
+static void help()
+{
+	// print a welcome message, and the OpenCV version
+	cout << "\nThis is a demo of Lukas-Kanade optical flow lkdemo(),\n"
+		"Using OpenCV version " << CV_VERSION << endl;
+	cout << "\nIt uses camera by default, but you can provide a path to video as an argument.\n";
+	cout << "\nHot keys: \n"
+		"\tESC - quit the program\n"
+		"\tr - auto-initialize tracking\n"
+		"\tc - delete all the points\n"
+		"\ts - set aim point\n"
+		"\tk - calibrate camera\n"
+		<< endl;
+}
 
 
 
-int main( int argc, char** argv )
+
+
+int main(int argc, char** argv)
 {
 
 	int wait_time = 0;
 
- //   help();
-    //cv::CommandLineParser parser(argc, argv, "{@input|0|}");
-    //string input = parser.get<string>("@input");
+	//   help();
+	   //cv::CommandLineParser parser(argc, argv, "{@input|0|}");
+	   //string input = parser.get<string>("@input");
 
 	VideoCapture cap;
 
 	//if (input.size() == 1 && isdigit(input[0]))
 	//{
-		
-		//cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-		//cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-		//cap.set(cv::CAP_PROP_FPS, 90); // OPTI
-	}
-    //else
-    //    cap.open(input);
+	//	cap.open(0);
+	//	//cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	//	//cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+	//	//cap.set(cv::CAP_PROP_FPS, 90); // OPTI
+	//}
+ //   else
+ //       cap.open(input);
 
 	cap.open(0);
 	cap.set(cv::CAP_PROP_FPS, 15);
 
-    if( !cap.isOpened() )
-    {
-        cout << "Could not initialize capturing...\n";
-        return 0;
-    }
+	if (!cap.isOpened())
+	{
+		cout << "Could not initialize capturing...\n";
+		return 0;
+	}
 
 	cout << "capturing initialised: " << cap.get(cv::CAP_PROP_FPS) << "  \n";
 
@@ -77,24 +81,26 @@ int main( int argc, char** argv )
 
 	// Hauptzyclus
 
-    for(;;)
-    {
+	for (;;)
+	{
 
 		if (udp_base.check_incoming_data())
 		{
+			cout << "new udp data \n";
+			cout << "aufnahme \n";
 
-			//imshow("LK Demo", *udp_base.get_frame_pointer());
-			//waitKey(100);
-			cout << "new udp data " << udp_base.check_incoming_data() << endl;
-			robot.new_data_proceed(&udp_base);
+			for (int i = 0; i < 6; i++) cap >> frame;
 
-			while (!udp_base.transfer_busy) 
+			if (cap.read(frame))
+			{
+				cout << "aufgenommen \n";
+			}
+			else
 			{
 				cout << "nicht aufgenommen \n";
 			}
-				cap >> frame;
 
-				cvtColor(frame, gray, COLOR_BGR2GRAY);
+			cvtColor(frame, gray, COLOR_BGR2GRAY);
 
 			udp_base.imagegrab_ready = true; // fuer thread mit Server
 
@@ -105,9 +111,9 @@ int main( int argc, char** argv )
 			imshow("LK Demo", frame);
 			waitKey(200);
 
+			robot.new_data_proceed(&udp_base);
 
-
-			while (udp_base.transfer_busy) 
+			while (udp_base.transfer_busy)
 			{
 				cout << "waiting transfer \r";
 			}
@@ -119,7 +125,7 @@ int main( int argc, char** argv )
 
 		//TODO skip frames
 
-    }
+	}
 
-    return 0;
+	return 0;
 }
